dd8cbfd99eadf9a84c4243f850a265a6fcb903e0
SCloves <clovesgtx@gmail.com>
2019-02-21 12:51:24 -0300

    usando input e output no obj Item

--- demo_project/items.py
+++ demo_project/items.py
@@ -6,9 +6,23 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+from w3lib.html import remove_tags
+
+def clean_quotation(value):
+	return value.replace(u"\u201c", "").replace(u"\u201d", "")
 
 
 class QuoteItem(scrapy.Item):
-    text = scrapy.Field()
-    author = scrapy.Field()
-    tags = scrapy.Field()
+    text = scrapy.Field(
+    	input_processor = MapCompose(str.strip, clean_quotation),
+    	output_processor = TakeFirst()
+    	)
+    author = scrapy.Field(
+    	input_processor = MapCompose(str.strip, remove_tags),
+    	output_processor = TakeFirst()
+    	)
+    tags = scrapy.Field(
+    	input_processor = MapCompose(remove_tags),
+    	output_processor = Join(',')
+    	)
